92336cf8de331fbd33c5a07a76116a4b0719df2a
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2016-10-06 11:24:46 -0600

    flask group assignments

---
+++ Velez_Felipe/assignments/flaskolympics/olympics5/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, session, request, redirect
+app = Flask(__name__)
+app.secret_key = 'my_secret_key'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process/<id>', methods = ['POST'])
+def process(id):
+    print ("I love flask! And {}".format(id))
+    return redirect('/')
+
+
+if __name__ == '__main__':
+  app.run(debug = True)
+
+
+"""
+Will this work? yes. What
+What will this print???
+I love flask! and peaches
+"""
---
+++ Velez_Felipe/assignments/flaskolympics/olympics7/server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, session, request, redirect
+import random
+
+app = Flask(__name__)
+app.secret_key = 'my_secret_key'
+
+@app.route('/')
+def index():
+    if not 'gold' in session:
+        session['gold'] = 0
+    if not 'activities' in session:
+        session['activities'] = []
+    return render_template('index.html')
+
+@app.route('/process', methods = ['POST'])
+def process():
+    buildings = {
+        'farm':random.randint(5,10),
+        'casino':random.randint(-50,50),
+        'cave':random.randint(0,30),
+        'house':random.randint(0,5)
+    }
+    if request.form['building'] in buildings:
+        print buildings[request.form['building']]
+    return redirect('/')
+
+
+if __name__ == '__main__':
+  app.run(debug = True)
+
+
+"""
+Explain line 24!
+line 24 returns a random value beween 5 and 10 generated from building-farm.
+"""
