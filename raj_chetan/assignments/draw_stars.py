8e8079a74c16c017e57a4ccae56e82500415807e
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2016-10-06 18:00:56 -0500

    deleted old assignments that really should be in there and committed 3 new assignments from oct 6... couner, dojo surey and great number game. submitted at 6pm.

--- raj_chetan/assignments/draw_stars.py
+++
@@ -1,9 +0,0 @@
-my_list =  [1,88,'butt',4,7,'chetan',10,25,'thecodingdojo']
-def drawStars(x):
-	for var in x:
-		if type(var) == int:
-			print '*'*var
-		else:
-			print var[0].lower()*len(var)
-
-drawStars(my_list)
